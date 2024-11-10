2a52858a8595c10b0a61e2aaf3aee25c9a2a2ead
saanv <saanv@DESKTOP-9IS6IPF>
2024-11-10 21:02:59 +0530

    initial files

---
+++ src/test/java/com/example/KarateTestRunner.java
@@ -0,0 +1,50 @@
+package com.example;
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.apache.commons.io.FileUtils;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import com.intuit.karate.junit4.Karate;
+
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+
+
+
+
+//@RunWith(Karate.class)  // JUnit 4 Runner for Karate tests
+public class KarateTestRunner {
+    // No need to manually call Karate.run here
+	
+	@Test
+	   public void testParallel() {
+	       Results results = Runner.path("classpath:com/example/api-tests.feature").tags("@done").outputCucumberJson(true).parallel(1);
+	       generateReport(results.getReportDir());
+	       System.out.println("this is "+results.getReportDir());
+	       assertTrue(results.getErrorMessages(), results.getFailCount() == 0);
+	       
+	        
+	        
+	   }
+	 public static void generateReport(String karateOutputPath) {
+	        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[]{"json"}, true);
+	        List<String> jsonPaths = new ArrayList(jsonFiles.size());
+	        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+	        Configuration config = new Configuration(new File("target"), "qa-automation");
+	        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+	        reportBuilder.generateReports();
+	    }
+	  
+}
+
+
+
